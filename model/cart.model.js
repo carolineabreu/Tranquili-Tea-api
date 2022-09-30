1fc3eb435c337b00e53ad7bb63f64a02c402df18
larissambn <larissameo1@gmail.com>
2022-09-30 14:54:33 -0300

    need to change order

--- model/cart.model.js
+++ model/cart.model.js
@@ -8,6 +8,7 @@ const cartSchema = new Schema({
   totalPrice: { type: Number, required: true },
   zipCode: { type: Number },
   dicountCoupons: { type: String },
+  // role: { type: String, enum: ["ADMIN", "USER"], default: "USER" },
 });
 
 export const cartModel = model("Cart", cartSchema);
--- model/order.model.js
+++ model/order.model.js
@@ -1,14 +1,15 @@
 import { Schema, model } from "mongoose";
 
 const orderSchema = new Schema({
- // id: { id },
-  status: { type: String, enum : ["", "", ""] required: true},
-  quantity: { type: Number, required : true},
-  priceTotal: { type: Number, required : true}, 
+  //id: { id },
+  status: { type: String, enum: ["", "", ""], required: true },
+  quantity: { type: Number, required: true },
+  priceTotal: { type: Number, required: true },
   passwordHash: { type: String, required: true },
   //teaID
   createdAt: { type: Date, default: Date.now() },
-  endedAt : { type: Date, default: // Date.now + 15() },
+  endedAt: { type: Date }, //default: // Date.now + 15() },
+  //role: { type: String, enum: ["ADMIN", "USER"], default: "USER" },
 });
 
 export const OrderModel = model("Order", orderSchema);
