d60214a226385ec5082c6d313651743deccc3008
Caroline Santos de Abreu <caroline@pop-os.localdomain>
2022-10-01 12:49:28 -0300

    user routes and model modiefied, forum modified

--- routes/forum.routes.js
+++ routes/forum.routes.js
@@ -1,30 +1,50 @@
 import express from "express";
+import isAuth from "../middlewares/isAuth.js";
+import attachCurrentUser from "../middlewares/attachCurrentUser.js";
+import { isAdmin } from "../middlewares/isAdmin.js";
 import { ForumModel } from "../model/forum.model.js";
+import { UserModel } from "../model/user.model.js";
 
 const forumRouter = express.Router();
 
-forumRouter.post("/", async (req, res) => {
-  try {
-    const createdPost = await ForumModel.create({ ...req.body });
+forumRouter.post(
+  "/new-post",
+  isAuth,
+  attachCurrentUser,
+  async (req, res) => {
+    try {
+      const loggedUser = req.currentUser;
 
-    return res.status(201).json(createdPost);
-  } catch (err) {
-    console.log(err);
-    return res.status(500).json(err);
-  }
-});
+      const post = await ForumModel.create({ ...req.body, owner: loggedUser._id });
+
+      await UserModel.findOneAndUpdate(
+        { _id: loggedUser._id },
+        { $push: {} }
+      );
+
+      return res.status(201).json(createdPost);
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json(err);
+    }
+  });
 
 //FIXME: essa rota vai pegar todos os posts do fórum ou do usuário?
-forumRouter.get("/", async (req, res) => {
-  try {
-    const allPosts = await ForumModel.find();
+forumRouter.get(
+  "/",
+  isAuth,
+  attachCurrentUser,
+  isAdmin,
+  async (req, res) => {
+    try {
+      const allPosts = await ForumModel.find();
 
-    return res.status(200).json(allPosts);
-  } catch (err) {
-    console.log(err);
-    return res.status(500).json(err);
-  }
-});
+      return res.status(200).json(allPosts);
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json(err);
+    }
+  });
 
 /**
  * TODO:
@@ -35,45 +55,57 @@ forumRouter.get("/", async (req, res) => {
 /** 
  * FIXME: se o MOD for pegar um post por id, ele pode pegar qualquer post, mas se for um usuário ele só pode ter acesso aos posts dele por id. O que muda? Ou não muda?
 */
-forumRouter.get("/:id", async (res, req) => {
-  try {
-    const post = await ForumModel.findOne({ _id: req.params.id });
+forumRouter.get(
+  "/:id",
+  isAuth,
+  attachCurrentUser,
+  async (res, req) => {
+    try {
+      const post = await ForumModel.findOne({ _id: req.params.id });
 
-    return res.status(200).json(post);
-  } catch (err) {
-    console.log(err);
-    return res.status(500).json(err);
-  }
-});
+      return res.status(200).json(post);
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json(err);
+    }
+  });
 
-forumRouter.put("/:id", async (res, req) => {
-  try {
-    const editPost = await ForumModel.findOneAndUpdate(
-      { _id: req.params.id },
-      { ...req.body },
-      { new: true, runValidators: true }
-    );
+forumRouter.put(
+  "/:id",
+  isAuth,
+  attachCurrentUser,
+  async (res, req) => {
+    try {
+      const editPost = await ForumModel.findOneAndUpdate(
+        { _id: req.params.id },
+        { ...req.body },
+        { new: true, runValidators: true }
+      );
 
-    return res.status(200).json(editPost);
-  } catch (err) {
-    console.log(err);
-    return res.status(500).json(err);
-  }
-});
+      return res.status(200).json(editPost);
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json(err);
+    }
+  });
 
 /**
  * TODO:
  * -[] se sobrar tempo colocar a opção de tanto o usuário poder deletar seu post, quando um MOD poder excluir algum post.
  */
-forumRouter.delete(":id", async (req, res) => {
-  try {
-    const deletePost = await ForumModel.deleteOne({ _id: req.params.id });
+forumRouter.delete(
+  ":id",
+  isAuth,
+  attachCurrentUser,
+  async (req, res) => {
+    try {
+      const deletePost = await ForumModel.deleteOne({ _id: req.params.id });
 
-    return res.status(200).json(deletePost);
-  } catch (err) {
-    console.log(err);
-    return res.status(500).json(err);
-  }
-});
+      return res.status(200).json(deletePost);
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json(err);
+    }
+  });
 
 export { forumRouter };
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { generateToken } from "../config/jwt.config.js";
-//import isAuth from "../middlewares/isAuth.js";
+import isAuth from "../middlewares/isAuth.js";
 import attachCurrentUser from "../middlewares/attachCurrentUser.js";
 import { isAdmin } from "../middlewares/isAdmin.js";
 import { UserModel } from "../model/user.model.js";
@@ -76,13 +76,15 @@ userRouter.post("/login", async (req, res) => {
 });
 
 userRouter.get(
-  "/teste",
-  //  isAuth,
+  "/profile",
+  isAuth,
   attachCurrentUser,
-  isAdmin,
   async (req, res) => {
+    const loggedUser = req.currentUser;
+    const userData = await UserModel.findOne({ _id: loggedUser._id }).populate("teas");
     return res.status(200).json(req.currentUser);
   }
 );
+
 
 export { userRouter };
