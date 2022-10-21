2dc3e4cc2cac0da7ace65f6864f9bcfa52b87868
Caroline Santos de Abreu <caroline@pop-os.localdomain>
2022-10-21 16:45:18 -0300

    fix forum post and user models

--- model/forumPost.model.js
+++ model/forumPost.model.js
@@ -2,7 +2,7 @@ import { Schema, model } from "mongoose";
 
 const forumPostSchema = new Schema({
   owner: { type: Schema.Types.ObjectId, ref: "User" },
-  title: { type: String, required: true, trim: true, minLength: 10, maxLength: 200 },
+  title: { type: String, required: true, trim: true, minLength: 5, maxLength: 200 },
   body: { type: String, required: true },
   createdAt: { type: Date, default: Date.now() },
   tag: { type: String, required: true, enum: ["Question", "Recommendation", "Blog", "Review", "Discussion"] },
